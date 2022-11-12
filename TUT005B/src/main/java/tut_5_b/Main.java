6dd0837c36a1398fbbaa80061163fec046eaebcd
manthilaDev <adeeshamanthila@outlook.com>
2022-11-12 13:22:14 +0530

    breakble code removed

--- TUT005B/src/main/java/tut_5_b/Main.java
+++ TUT005B/src/main/java/tut_5_b/Main.java
@@ -3,7 +3,6 @@ package tut_5_b;
 public class Main {
     public static void main(String[] args) {
 
-        MailBox mailBox = new MailBox();
         IMailBox multiSlotMailBox = new MultiSlotMailBox();
         Thread consumer = new Consumer("Consumer",multiSlotMailBox);
         Thread producer = new Producer("Producer",multiSlotMailBox);
